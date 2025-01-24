271c2bbfc52f05a1493c71540330e172be9b66d2
Anshul Raj Verma <arv.anshul.1864@gmail.com>
2025-01-24 19:10:07 +0530

    update altiusrt/spiders/match_details.py

--- src/altiusrt/spiders/match_details.py
+++ src/altiusrt/spiders/match_details.py
@@ -11,19 +11,26 @@ if typing.TYPE_CHECKING:
     from scrapy.http.response import Response
 
 
-class _MatchTeam(BaseModel):
-    id: int
-    name: str
-    code: str
-    organization_id: int
-
-
 class _MatchOfficial(BaseModel):
     id: int
     person_id: int
     role: str
     displayname: str
     medianame: str
+
+
+class _MatchGoal(BaseModel):
+    id: int
+    match_id: int
+    seconds: float
+    team_id: int
+    event: str
+    player_id: int
+    defender_id: int | None = None
+    official_id: int | None = None
+    type: str | None = None
+    outcome: str | None = None
+    minutedisp: str
 
 
 class MatchDetailsModel(BaseModel):
@@ -48,8 +55,7 @@ class MatchDetailsModel(BaseModel):
     period_minutes: float
     period_count: int
     shootout_count: int
-    hometeam: _MatchTeam
-    awayteam: _MatchTeam
+    goals: list[_MatchGoal]
     statistics: dict[str, typing.Any]
 
 
@@ -69,7 +75,7 @@ class MatchesDetailsSpider(scrapy.Spider):
 
     def start_requests(self):
         for match_id in self.match_ids:
-            url = f"https://{self.domain}/rt/matches/{match_id}?embeds=statistics,hometeam,awayteam"
+            url = f"https://{self.domain}/rt/matches/{match_id}?embeds=statistics,goals"
             yield Request(url, self.parse)
 
     def parse(self, response: Response):
